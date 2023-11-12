f373ad3e9bbb36f49b2c636cc3c87f6366d9149f
Igor Brito <114553952+IgorBrito02@users.noreply.github.com>
2023-11-12 17:29:36 -0300

    Add files via upload

---
+++ construtoraFBCC/menu.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-sm bg-dark p-3 navbar-dark justify-content-center">
+    <ul class="navbar-nav">
+    <li class="nav-item">
+        <a class="nav-link active" href="?pg=principal">Home</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="?pg=quemsomos">Quem Somos</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="?pg=produtos">Produtos</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="?pg=servicos">Serviços</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="?pg=faleconosco">Fale conosco</a>
+        </li>
+    </ul>
+</nav>
---
+++ construtoraFBCC/rodape.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container-fluid p-5 bg-primary text-white text-center">
+        2020 &copy; MeuSite.com - Direitos Reservados
+    </div>
+</footer>
